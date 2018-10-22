78ba8fa536883dbcc9859d30001d93abbb7bc99b
Vyas Swaroop <vyasswaroop@FullContacts-MacBook-Pro.local>
2018-10-22 22:47:24 +0530

    Added steps to initialise environment variables for virtualenv wrapper inside .zshrc

--- install_basics.sh
+++ install_basics.sh
@@ -3,27 +3,33 @@
 # Install brew package
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install zshell 
-brew install zsh 
+# Install zshell
+brew install zsh
 
-# Install Ohmyzsh theme
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+# Install Ohmyzshtheme
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install VSCode 
-brew cask install visual-studio-code 
+# Install VSCode
+brew cask install visual-studio-code
 
-# Install Python2 and Python3 
-brew install python@2 
-brew install python3 
+# Install Python2 and Python3
+brew install python@2
+brew install python3
 
-# Install Virtualenv and virtualenv wrapper 
-brew install virtualenv brew install virtualenvwrapper 
+# Install Virtualenv and virtualenv wrapper
+brew install virtualenv
+brew install virtualenvwrapper
+cat >>~/.zshrc <<EOL
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
+EOL
 
-# Install Google chrome 
-brew cask install google-chrome 
+# Install Google chrome
+brew cask install google-chrome
 
-# Install Slack app 
-brew cask install slack 
+# Install Slack app
+brew cask install slack
 
-# Install Docker 
+# Install Docker
 brew cask install docker
