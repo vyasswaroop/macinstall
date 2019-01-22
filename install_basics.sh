49a5dc20965e4bdf4f377fef73806038c5414718
Vyas Swarooop <vyasswaroop.mec@gmail.com>
2019-01-22 15:21:13 +0530

    Updation with current apps

--- install_basics.sh
+++ install_basics.sh
@@ -3,20 +3,14 @@
 # Install brew package
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install zshell
+# Setting up Terminal
+brew cask install iterm2
 brew install zsh
-
-# Install Ohmyzshtheme
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install VSCode
-brew cask install visual-studio-code
-
-# Install Python2 and Python3
+# Install python and virtualenvironment
 brew install python@2
 brew install python3
-
-# Install Virtualenv and virtualenv wrapper
 brew install virtualenv
 brew install virtualenvwrapper
 cat >>~/.zshrc <<EOL
@@ -25,11 +19,12 @@ export PROJECT_HOME=$HOME/Devel
 source /usr/local/bin/virtualenvwrapper.sh
 EOL
 
-# Install Google chrome
+# Install work utilities
+brew cask install visual-studio-code
 brew cask install google-chrome
-
-# Install Slack app
 brew cask install slack
-
-# Install Docker
 brew cask install docker
+brew cask install alfred
+brew cask install insomnia
+brew cask install bear
+brew cask install vlc
