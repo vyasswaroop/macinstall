d8d97729c22d3a9f5a1228380067259d3d98ec56
vyasswaroop <vyasswaroop.mec@gmail.com>
2019-07-24 18:42:30 +0530

    Update apps

--- install_basics.sh
+++ install_basics.sh
@@ -6,7 +6,8 @@
 # Setting up Terminal
 brew cask install iterm2
 brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+source ~/.zshrc
 
 # Install python and virtualenvironment
 brew install python@2
@@ -18,6 +19,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
 source /usr/local/bin/virtualenvwrapper.sh
 EOL
+brew install awscli
 
 # Install work utilities
 brew cask install visual-studio-code
@@ -26,5 +28,9 @@ brew cask install slack
 brew cask install docker
 brew cask install alfred
 brew cask install insomnia
-brew cask install bear
+brew cask install tunnelblick
+brew cask install ngrok
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install authy
 brew cask install vlc
